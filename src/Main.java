7291a97d0e5fe6b417d276b358bb1de29f4a1065
Olga Kapustina <hellog@rambler.ru>
2022-05-25 23:55:47 +0300

    src

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 17_236;
+        int milesPrice = 20;
+        int milesEarned = (int) price / milesPrice;
+        System.out.println(milesEarned);
+    }
+}
